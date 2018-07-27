d0ac725a3b0fdd9f9e54496bf63032fd422f99e2
Chloe <32880794+chequet@users.noreply.github.com>
2018-07-27 14:52:34 +0100

    added multiple folder handling and aam4 now trains on more data

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 import menpo.io as mio
 from conversion import landmarkConverter
+from os import walk
 
 def process(image, crop_proportion=0.2, max_diagonal=400):
     path = str(image.path)
-    lpath = s.replace('.png','_aam.txt')
+    lpath = path.replace('.png','_aam.txt').replace('Images','AAM_landmarks')
     lm = landmarkConverter(lpath)
     image.landmarks = lm
     if image.n_channels == 3:
@@ -14,4 +15,14 @@ def process(image, crop_proportion=0.2, max_diagonal=400):
         image = image.rescale(float(max_diagonal) / d)
     return image
 
-
+def prepare_images(folderpath):
+	training_paths = []
+	training_images = []
+	for root, dirs, files in walk(folderpath):
+			if len(files) > 0:
+				training_paths.append(root)
+	for path in training_paths:
+		images = mio.import_images(path, verbose=True)
+		images = images.map(process)
+		training_images += images
+	return training_images 
